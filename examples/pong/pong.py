61508a97ccc2c130db741ebafdb5f9fbf22d67a2
Manuel Cerón <ceronman@gmail.com>
2011-09-28 18:36:01 -0500

    Adding movement to the ball.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -1,4 +1,4 @@
-from pyglet import app, resource
+from pyglet import app, resource, clock
 from pyglet.sprite import Sprite
 from pyglet.window import Window
 
@@ -8,10 +8,16 @@ ball_image = resource.image('images/ball.png')
 ball = Sprite(ball_image)
 ball.x = window.width / 2 - ball.width/2
 ball.y = window.height / 2 - ball.width/2
+ball.speed = 50
 
 @window.event
 def on_draw():
     window.clear()
     ball.draw()
+
+def update(dt):
+    ball.x = ball.x + ball.speed * dt
+
+clock.schedule_interval(update, 1/60.0)
 
 app.run()
