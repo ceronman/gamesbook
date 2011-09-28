59399a9252578615206f26d150ad36c0e58ff58b
Manuel Cerón <ceronman@gmail.com>
2011-09-28 18:28:51 -0500

    Placing the ball at the center of the screen.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -6,6 +6,8 @@ window = Window()
 
 ball_image = resource.image('images/ball.png')
 ball = Sprite(ball_image)
+ball.x = window.width / 2 - ball.width/2
+ball.y = window.height / 2 - ball.width/2
 
 @window.event
 def on_draw():
