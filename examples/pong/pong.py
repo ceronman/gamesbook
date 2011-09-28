a867a4d3848d66f1c56204bb97b5dbce9264258e
Manuel Cerón <ceronman@gmail.com>
2011-09-28 18:25:33 -0500

    Drawing a simple ball image.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -1,6 +1,15 @@
+from pyglet import app, resource
+from pyglet.sprite import Sprite
 from pyglet.window import Window
-from pyglet import app
 
 window = Window()
+
+ball_image = resource.image('images/ball.png')
+ball = Sprite(ball_image)
+
+@window.event
+def on_draw():
+    window.clear()
+    ball.draw()
 
 app.run()
