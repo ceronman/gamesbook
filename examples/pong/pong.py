27277b3aae35dee6dbc0d6e4c465ddb4cfb61c82
Manuel Cerón <ceronman@gmail.com>
2011-09-28 18:42:41 -0500

    Adding ball bouncing.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -8,7 +8,8 @@ ball_image = resource.image('images/ball.png')
 ball = Sprite(ball_image)
 ball.x = window.width / 2 - ball.width/2
 ball.y = window.height / 2 - ball.width/2
-ball.speed = 50
+ball.speed_x = 50
+ball.speed_y = 50
 
 @window.event
 def on_draw():
@@ -16,7 +17,14 @@ def on_draw():
     ball.draw()
 
 def update(dt):
-    ball.x = ball.x + ball.speed * dt
+    ball.x = ball.x + ball.speed_x * dt
+    ball.y = ball.y + ball.speed_y * dt
+
+    if ball.x < 0  or (ball.x +  ball.width) > window.width:
+        ball.speed_x = ball.speed_x * -1
+
+    if ball.y < 0 or (ball.y + ball.width) > window.height:
+        ball.speed_y = ball.speed_y * -1
 
 clock.schedule_interval(update, 1/60.0)
 
