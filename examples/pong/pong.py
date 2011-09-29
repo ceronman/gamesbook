9cde46c566f2f3e19bb3cdf116e80946c30fe4d8
Manuel Cerón <ceronman@gmail.com>
2011-09-28 19:11:46 -0500

    Adding movement to the paddles.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -1,15 +1,17 @@
 from pyglet import app, resource, clock
 from pyglet.sprite import Sprite
-from pyglet.window import Window
+from pyglet.window import Window, key
 
 window = Window()
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
 
 ball_image = resource.image('images/ball.png')
 ball = Sprite(ball_image)
 ball.x = window.width / 2 - ball.width/2
 ball.y = window.height / 2 - ball.width/2
-ball.speed_x = 50
-ball.speed_y = 50
+ball.speed_x = 200
+ball.speed_y = 200
 
 PADDLE_MARGIN = 10
 
@@ -17,9 +19,11 @@ paddle_image = resource.image('images/paddle.png')
 paddle1 = Sprite(paddle_image)
 paddle1.x = PADDLE_MARGIN
 paddle1.y = window.height / 2 - paddle1.height / 2
+paddle1.speed = 200
 paddle2 = Sprite(paddle_image)
 paddle2.x = window.width - paddle2.width - PADDLE_MARGIN
 paddle2.y = window.height / 2 - paddle2.height / 2
+paddle2.speed = 200
 
 @window.event
 def on_draw():
@@ -38,6 +42,18 @@ def update(dt):
 
     if ball.y < 0 or (ball.y + ball.width) > window.height:
         ball.speed_y = ball.speed_y * -1
+
+    if keys[key.A]:
+        paddle1.y = paddle1.y + paddle1.speed * dt
+
+    if keys[key.Z]:
+        paddle1.y = paddle1.y - paddle1.speed * dt
+
+    if keys[key.UP]:
+        paddle2.y = paddle2.y + paddle2.speed * dt
+
+    if keys[key.DOWN]:
+        paddle2.y = paddle2.y - paddle2.speed * dt
 
 clock.schedule_interval(update, 1/60.0)
 
