98e3a61ef1f9ffbadfda89388431b4510d0c90a9
Manuel Cerón <ceronman@gmail.com>
2011-09-28 19:00:36 -0500

    Adding two paddles.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -11,10 +11,23 @@ ball.y = window.height / 2 - ball.width/2
 ball.speed_x = 50
 ball.speed_y = 50
 
+PADDLE_MARGIN = 10
+
+paddle_image = resource.image('images/paddle.png')
+paddle1 = Sprite(paddle_image)
+paddle1.x = PADDLE_MARGIN
+paddle1.y = window.height / 2 - paddle1.height / 2
+paddle2 = Sprite(paddle_image)
+paddle2.x = window.width - paddle2.width - PADDLE_MARGIN
+paddle2.y = window.height / 2 - paddle2.height / 2
+
 @window.event
 def on_draw():
     window.clear()
     ball.draw()
+    paddle1.draw()
+    paddle2.draw()
+
 
 def update(dt):
     ball.x = ball.x + ball.speed_x * dt
