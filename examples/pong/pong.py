3721696fb49cb9482210606a0989f3a73dfb438d
Manuel Cerón <ceronman@gmail.com>
2011-10-02 21:02:01 -0500

    Added game pause with P key.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -4,8 +4,6 @@ from pyglet.text import Label
 from pyglet.window import Window, key
 
 window = Window()
-keys = key.KeyStateHandler()
-window.push_handlers(keys)
 
 ball_image = resource.image('images/ball.png')
 ball = Sprite(ball_image)
@@ -43,64 +41,89 @@ score_label_2 = Label('0',
                       anchor_x = 'center',
                       anchor_y = 'top')
 
+state = 'playing'
+
+pause_label = Label('PAUSED',
+                    font_size=36,
+                    x = window.width/2,
+                    y = window.height/2,
+                    anchor_x = 'center', anchor_y = 'center')
+
 @window.event
 def on_draw():
     window.clear()
-    ball.draw()
-    paddle1.draw()
-    paddle2.draw()
-    score_label_1.draw()
-    score_label_2.draw()
+    if state == 'playing':
+        ball.draw()
+        paddle1.draw()
+        paddle2.draw()
+        score_label_1.draw()
+        score_label_2.draw()
+    if state == 'paused':
+        pause_label.draw()
 
+@window.event
+def on_key_press(symbol, modifiers):
+    global state
+    if symbol == key.P:
+        state = 'paused' if state == 'playing' else 'playing'
+
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
 
 def update(dt):
-    ball.x = ball.x + ball.speed_x * dt
-    ball.y = ball.y + ball.speed_y * dt
+    global state
 
-    if ball.x < (PADDLE_MARGIN + paddle1.width):
-        ball.x = PADDLE_MARGIN + paddle1.width
-        if ((ball.y + ball.height) > paddle1.y
-            and (ball.y - ball.height) < (paddle1.y + paddle1.height)):
-            ball.speed_x = ball.speed_x * -1
-        else:
-            global score_2
-            score_2 = score_2 + 1
-            score_label_2.text = str(score_2)
-            ball.x = window.width / 2
-            ball.y = window.height / 2
+    if state == 'paused':
+        return
 
-    if (ball.x + ball.width) > paddle2.x:
-        ball.x = paddle2.x - ball.width
-        if ((ball.y + ball.height) > paddle2.y
-            and (ball.y - ball.height) < (paddle2.y + paddle2.height)):
-            ball.speed_x = ball.speed_x * -1
-        else:
-            global score_1
-            score_1 = score_1 + 1
-            score_label_1.text = str(score_1)
-            ball.x = window.width / 2
-            ball.y = window.height / 2
+    elif state == 'playing':
+        ball.x = ball.x + ball.speed_x * dt
+        ball.y = ball.y + ball.speed_y * dt
 
-    if (ball.y + ball.height) > window.height:
-        ball.y = window.height - ball.height
-        ball.speed_y = ball.speed_y * -1
+        if ball.x < (PADDLE_MARGIN + paddle1.width):
+            ball.x = PADDLE_MARGIN + paddle1.width
+            if ((ball.y + ball.height) > paddle1.y
+                and (ball.y - ball.height) < (paddle1.y + paddle1.height)):
+                ball.speed_x = ball.speed_x * -1
+            else:
+                global score_2
+                score_2 = score_2 + 1
+                score_label_2.text = str(score_2)
+                ball.x = window.width / 2
+                ball.y = window.height / 2
 
-    if ball.y < 0:
-        ball.y = 0
-        ball.speed_y = ball.speed_y * -1
+        if (ball.x + ball.width) > paddle2.x:
+            ball.x = paddle2.x - ball.width
+            if ((ball.y + ball.height) > paddle2.y
+                and (ball.y - ball.height) < (paddle2.y + paddle2.height)):
+                ball.speed_x = ball.speed_x * -1
+            else:
+                global score_1
+                score_1 = score_1 + 1
+                score_label_1.text = str(score_1)
+                ball.x = window.width / 2
+                ball.y = window.height / 2
+
+        if (ball.y + ball.height) > window.height:
+            ball.y = window.height - ball.height
+            ball.speed_y = ball.speed_y * -1
+
+        if ball.y < 0:
+            ball.y = 0
+            ball.speed_y = ball.speed_y * -1
 
 
-    if keys[key.A] and paddle1.y < (window.height - paddle1.height):
-        paddle1.y = paddle1.y + paddle1.speed * dt
+        if keys[key.A] and paddle1.y < (window.height - paddle1.height):
+            paddle1.y = paddle1.y + paddle1.speed * dt
 
-    if keys[key.Z] and paddle1.y > 0:
-        paddle1.y = paddle1.y - paddle1.speed * dt
+        if keys[key.Z] and paddle1.y > 0:
+            paddle1.y = paddle1.y - paddle1.speed * dt
 
-    if keys[key.UP] and paddle2.y < (window.height - paddle2.height):
-        paddle2.y = paddle2.y + paddle2.speed * dt
+        if keys[key.UP] and paddle2.y < (window.height - paddle2.height):
+            paddle2.y = paddle2.y + paddle2.speed * dt
 
-    if keys[key.DOWN] and paddle2.y > 0:
-        paddle2.y = paddle2.y - paddle2.speed * dt
+        if keys[key.DOWN] and paddle2.y > 0:
+            paddle2.y = paddle2.y - paddle2.speed * dt
 
 clock.schedule_interval(update, 1/60.0)
 
