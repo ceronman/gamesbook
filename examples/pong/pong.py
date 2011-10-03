4abb0929f04a2b86724c99066b298aadec59a6cd
Manuel Cerón <ceronman@gmail.com>
2011-10-02 19:37:28 -0500

    Added score labels.

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -1,5 +1,6 @@
 from pyglet import app, resource, clock
 from pyglet.sprite import Sprite
+from pyglet.text import Label
 from pyglet.window import Window, key
 
 window = Window()
@@ -25,12 +26,31 @@ paddle2.x = window.width - paddle2.width - PADDLE_MARGIN
 paddle2.y = window.height / 2 - paddle2.height / 2
 paddle2.speed = 200
 
+score_1 = 0
+score_2 = 0
+
+score_label_1 = Label('0',
+                      font_size=48,
+                      x = window.width/4,
+                      y = window.height,
+                      anchor_x = 'center',
+                      anchor_y = 'top')
+
+score_label_2 = Label('0',
+                      font_size=48,
+                      x = 3 * window.width/4,
+                      y = window.height,
+                      anchor_x = 'center',
+                      anchor_y = 'top')
+
 @window.event
 def on_draw():
     window.clear()
     ball.draw()
     paddle1.draw()
     paddle2.draw()
+    score_label_1.draw()
+    score_label_2.draw()
 
 
 def update(dt):
@@ -43,6 +63,9 @@ def update(dt):
             and (ball.y - ball.height) < (paddle1.y + paddle1.height)):
             ball.speed_x = ball.speed_x * -1
         else:
+            global score_2
+            score_2 = score_2 + 1
+            score_label_2.text = str(score_2)
             ball.x = window.width / 2
             ball.y = window.height / 2
 
@@ -52,6 +75,9 @@ def update(dt):
             and (ball.y - ball.height) < (paddle2.y + paddle2.height)):
             ball.speed_x = ball.speed_x * -1
         else:
+            global score_1
+            score_1 = score_1 + 1
+            score_label_1.text = str(score_1)
             ball.x = window.width / 2
             ball.y = window.height / 2
 
